75ea481cc0d9fe92260a5fc3bdf6066608607af4
Xavier Mayeur <xavier@mayeur.be>
2019-07-31 21:05:33 +0200

    Initial

---
+++ tdMQTTbridge.py
@@ -0,0 +1,355 @@
+# -*- coding: utf-8 -*-
+import getopt
+import json
+import logging
+import sys
+from logging.handlers import RotatingFileHandler
+import configparser
+import requests
+from six.moves.urllib.parse import urlencode
+from configobj import ConfigObj
+from requests_oauthlib import OAuth1
+import paho.mqtt.client as mqtt
+from time import sleep
+
+project = 'tdMQTTbridge'
+LOG_file = project+'.log'
+INI_file = project+'.conf'
+connect_flag = False
+
+def open_log(name):
+    # Setup the log handlers to stdout and file.
+    log_ = logging.getLogger(name)
+    log_.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s | %(name)s | %(levelname)s | %(message)s'
+    )
+    handler_stdout = logging.StreamHandler(sys.stdout)
+    handler_stdout.setLevel(logging.DEBUG)
+    handler_stdout.setFormatter(formatter)
+    log_.addHandler(handler_stdout)
+    handler_file = RotatingFileHandler(
+        LOG_file,
+        mode='a',
+        maxBytes=200000,
+        backupCount=9,
+        encoding='UTF-8',
+        delay=True
+    )
+    handler_file.setLevel(logging.DEBUG)
+    handler_file.setFormatter(formatter)
+    log_.addHandler(handler_file)
+    return log_
+
+
+log = open_log(project)
+
+
+def open_config(f):
+    global log
+    log = open_log(project + '.open_config')
+    # Read config file - halt script on failure
+    config_ = None
+    try:
+        with open(f, 'r+') as config_file:
+            config_ = configparser.ConfigParser()
+            config_.read_file(config_file)
+    except IOError:
+        pass
+    if config_ is None:
+        log.critical('configuration file is missing')
+    return config_
+
+
+# Open config file
+config = open_config(INI_file)
+
+
+def get_vault(uid):
+    url = config.get('vault', 'vault_url')
+    r = requests.get(url=url + '?uid=%s' % uid)
+    _id = r.json()
+    r.close()
+
+    if _id['status'] == 200:
+        _username = _id['username']
+        _password = _id['password']
+    else:
+        _username = ''
+        _password = ''
+    return _username, _password
+
+
+telldus_key = config.get('telldus', 'client')
+telldus_apptoken = config.get('telldus', 'token')
+PUBLIC_KEY, PRIVATE_KEY = get_vault(telldus_key)
+TOKEN, TOKEN_SECRET = get_vault(telldus_apptoken)
+
+TELLSTICK_TURNON = 1
+TELLSTICK_TURNOFF = 2
+TELLSTICK_BELL = 4
+TELLSTICK_DIM = 16
+TELLSTICK_UP = 128
+TELLSTICK_DOWN = 256
+
+RASPI_ID = 274164
+SALON_ID = 223659
+SAM_ID = 223659
+NAS_ID = 274165
+
+SUPPORTED_METHODS = TELLSTICK_TURNON | TELLSTICK_TURNOFF | TELLSTICK_BELL | TELLSTICK_DIM | TELLSTICK_UP | TELLSTICK_DOWN
+
+
+def do_mqtt_connect(client, host):
+    global connect_flag
+    try:
+        client.connect(host, port=1883)
+        while not client.connected_flag:
+            print('+')
+            sleep(1)
+    
+    except mqtt.MQTT_ERR_ACL_DENIED:
+        print('Invalid username or password')
+        log.critical('Invalid username or password')
+    
+    except Exception as e:
+        print('Cannot connect to mqtt broker -  error: ' + str(e))
+        log.critical('Cannot connect to mqtt broker - retrying')
+        client.connected_flag = False
+
+
+def do_mqtt_publish(client, key, value, qos=0, retain=False):
+    try:
+        client.publish(project + '/' + key, str(value), qos=0, retain=False)
+        return True
+    except Exception as e:
+        print('Cannot publish to mqtt broker -  error: ' + str(e))
+        log.critical('Cannot publish to mqtt broker - retrying')
+        return False
+        
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("connected ok")
+        client.connected_flag = True
+
+
+def on_message(client, userdata, message):
+    topic = message.topic
+    msg = str(message.payload.decode('utf-8'))
+    print('Received message: ' + topic + '/' + msg)
+    if topic == project + '/getStatus':
+        do_mqtt_publish(project + '/status', 'alive', qos=0, retain=False)
+
+
+
+def listDevices():
+
+    response = doRequest('devices/list', {'supportedMethods': SUPPORTED_METHODS})
+    # print("Number of devices: %i" % len(response['device']))
+    for device in response['device']:
+        if device['state'] == TELLSTICK_TURNON:
+            state = 'ON'
+        elif device['state'] == TELLSTICK_TURNOFF:
+            state = 'OFF'
+        elif device['state'] == TELLSTICK_DIM:
+            state = "DIMMED"
+        elif device['state'] == TELLSTICK_UP:
+            state = "UP"
+        elif device['state'] == TELLSTICK_DOWN:
+            state = "DOWN"
+        else:
+            state = 'Unknown state'
+        # print ("%s\t%s\t%s" % (device['id'], device['name'], state))
+    return json.dumps(response['device'], indent=4, separators=(',', ': '))
+
+
+
+def publishSensors(client):
+    response = doRequest('sensors/list', {'includeIgnored': 0})
+    # print("Number of sensors: %i" % len(response['device']))
+    for sensor in response['sensor']:
+        resp2 = doRequest('sensor/info', {'id': sensor['id']})
+        
+        r = do_mqtt_publish(client, resp2['name'], json.dumps(resp2), qos=0, retain=False)
+        # print ("%s\t%s\t%s" % (sensor['id'], sensor['name'], state))
+        if not r:
+            return False
+    return True
+
+def publishDevices(client):
+    response = doRequest('devices/list', {'supportedMethods': SUPPORTED_METHODS})
+    # print("Number of devices: %i" % len(response['device']))
+    for device in response['device']:
+        r = do_mqtt_publish(client, device['name'], json.dumps(device), qos=0, retain=False)
+        # print ("%s\t%s\t%s" % (device['id'], device['name'], state))
+        if not r:
+            return False
+    return True
+
+
+# def doJob(id, deviceId, methodId, methodValue=0, type='time', hour, minute, weekdays)
+#    response = doRequest('scheduler/setJob', {'id':  id, 'deviceId': deviceId, 'method': methodId,
+#    'methodValue': methodValue, 'type': type, 'hour': hour, 'minute': minute, 'weekdays': weekdays})
+
+
+def getDeviceState(deviceID):
+    response = doRequest('device/info', {'id': deviceID, 'supportedMethods': 255})
+    val = int(response['state'])
+    val2 = str(response['statevalue'])
+    
+    if val == TELLSTICK_TURNON:
+        state = 'ON'
+    elif val == TELLSTICK_TURNOFF:
+        state = 'OFF'
+    elif val == TELLSTICK_DIM:
+        state = val2
+    elif val == TELLSTICK_UP:
+        state = "UP"
+    elif val == TELLSTICK_DOWN:
+        state = "DOWN"
+    else:
+        state = 'Unknown state'
+    return state
+
+
+def switchRpiOff():
+    doMethod(SALON_ID, TELLSTICK_TURNOFF)
+
+
+def doMethod(deviceId, methodId, methodValue=0):
+    response = doRequest('device/info', {'id': deviceId})
+    
+    if methodId == TELLSTICK_TURNON:
+        method = 'on'
+    elif methodId == TELLSTICK_TURNOFF:
+        method = 'off'
+    elif methodId == TELLSTICK_BELL:
+        method = 'bell'
+    elif methodId == TELLSTICK_UP:
+        method = 'up'
+    elif methodId == TELLSTICK_DOWN:
+        method = 'down'
+    elif methodId == TELLSTICK_DIM:
+        method = 'dim'
+    
+    if 'error' in response:
+        name = ''
+        retString = response['error']
+    else:
+        name = response['name']
+        response = doRequest('device/command', {'id': deviceId, 'method': methodId, 'value': methodValue})
+        if 'error' in response:
+            retString = response['error']
+        else:
+            retString = response['status']
+    
+    if methodId in (TELLSTICK_TURNON, TELLSTICK_TURNOFF):
+        # print ("Turning %s device %s, %s - %s" % (method, deviceId, name, retString))
+        return retString
+    elif methodId in (TELLSTICK_BELL, TELLSTICK_UP, TELLSTICK_DOWN):
+        # print("Sending %s to: %s %s - %s" % (method, deviceId, name, retString))
+        return retString
+    elif methodId == TELLSTICK_DIM:
+        # print ("Dimming device: %s %s to %s - %s" % (deviceId, name, methodValue, retString))
+        return retString
+
+
+def doRequest(method, params):
+    http_url = "http://api.telldus.com/json/" + method + "?" + urlencode(params, True).replace('+', '%20')
+    oauth = OAuth1(PUBLIC_KEY, PRIVATE_KEY, TOKEN, TOKEN_SECRET)
+    r = requests.get(http_url, auth=oauth)
+    return r.json()
+
+
+def requestToken():
+    return
+    # global config
+    #
+    # consumer = oauth.Consumer(PUBLIC_KEY, PRIVATE_KEY)
+    # request = oauth.Request.from_consumer_and_token(consumer, http_url='http://api.telldus.com/oauth/requestToken')
+    # request.sign_request(oauth.SignatureMethod_HMAC_SHA1(), consumer, None)
+    # conn = http.client.HTTPConnection('api.telldus.com:80')
+    # conn.request(request.http_method, '/oauth/requestToken', headers=request.to_header())
+    #
+    # resp = conn.getresponse().read()
+    # token = oauth.Token.from_string(resp)
+    # print((
+    #         'Open the following url in your webbrowser:\nhttp://api.telldus.com/oauth/authorize?oauth_token=%s\n' % token.key))
+    # print(('After logging in and accepting to use this application run:\n%s --authenticate' % (sys.argv[0])))
+    # config['telldus']['requestToken'] = str(token.key)
+    # config['telldus']['requestTokenSecret'] = str(token.secret)
+    # saveConfig()
+
+
+def getAccessToken():
+    return
+    # global config
+    # consumer = oauth.Consumer(PUBLIC_KEY, PRIVATE_KEY)
+    # token = oauth.Token(config['telldus']['requestToken'], config['telldus']['requestTokenSecret'])
+    # request = oauth.Request.from_consumer_and_token(consumer, token=token, http_method='GET',
+    #                                                 http_url='http://api.telldus.com/oauth/accessToken')
+    # request.sign_request(oauth.SignatureMethod_HMAC_SHA1(), consumer, token)
+    # conn = http.client.HTTPConnection('api.telldus.com:80')
+    # conn.request(request.http_method, request.to_url(), headers=request.to_header())
+    #
+    # resp = conn.getresponse()
+    # if resp.status != 200:
+    #     print(('Error retrieving access token, the server replied:\n%s' % resp.read()))
+    #     return
+    # token = oauth.Token.from_string(resp.read())
+    # config['telldus']['requesttoken'] = None
+    # config['telldus']['requesttokensecret'] = None
+    # config['telldus']['token'] = str(token.key)
+    # config['telldus']['tokentecret'] = str(token.secret)
+    # print('Authentication successful, you can now use tdtool')
+    # saveConfig()
+
+
+def authenticate():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], '', ['authenticate'])
+        for opt, arg in opts:
+            if opt in '--authenticate':
+                getAccessToken()
+                return
+    except getopt.GetoptError:
+        pass
+    requestToken()
+
+
+def main():
+    global connect_flag
+    
+    mqtt.Client.connected_flag = False
+    # Connect to mqtt bus
+    uid = config.get('mqtt', 'uid')
+    host = config.get('mqtt', 'host')
+    
+    uname, pwd = get_vault(uid)
+    client = mqtt.Client(project)
+    client.username_pw_set(username=uname, password=pwd)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    duration = int(config.get('mqtt', 'duration'))
+    client.loop_start()
+    do_mqtt_connect(client, host)
+    client.subscribe(project + '/getStatus')
+  
+    while True:
+        # Get device list ans state
+        r = publishDevices(client)
+        if not r:
+            do_mqtt_connect(client, host)
+            r = publishDevices(client)
+
+        r = publishSensors(client)
+        if not r:
+            do_mqtt_connect(client, host)
+            r = publishSensors(client)
+        sleep(duration)
+    
+
+if __name__ == "__main__":
+    main()
+
