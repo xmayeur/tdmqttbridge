4c5ff4e26a3d0a3beb9e2cfaa50217d7dfcdfe6f
Xavier Mayeur <xavier@mayeur.be>
2019-08-05 22:59:04 +0200

    bug fix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 NAME="tdmqttbridge"
 docker rm -f $NAME
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
+# docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build -t $NAME .
 docker tag $NAME xmayeur/$NAME
 docker run -it --name $NAME --network=host  --restart always xmayeur/$NAME
