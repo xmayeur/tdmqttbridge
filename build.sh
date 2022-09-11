d7a817a5272afb7d32b55e0ce5970190a3fb7d19
XY56RE <xavier.mayeur@ing.com>
2022-09-11 22:04:00 +0200

    fix bugs

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 NAME="tdmqttbridge"
 docker rm -f $NAME
 # docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker build -t $NAME .
+docker build -t $NAME -f Dockerfile-arm7 .
 docker tag $NAME xmayeur/$NAME
 docker push xmayeur/$NAME
 docker run -it --name $NAME --network=host  --restart always xmayeur/$NAME
